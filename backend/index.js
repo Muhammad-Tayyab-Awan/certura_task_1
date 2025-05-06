fa16901d7d5b56b24870a85665196a2c21e41bdc
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-06 10:09:33 +0500

    added cors body-parser and cookie-parser middleware and db connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-undef */
 import "dotenv/config";
 import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import connectDB from "./utils/connectDB.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.listen(port, async () => {
   console.clear();
+  await connectDB();
   console.log(`Server running on http://localhost:${port}`);
 });
