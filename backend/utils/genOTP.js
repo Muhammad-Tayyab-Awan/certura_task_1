551ab04d26d1656dd5afc46433e1bd7de51c7cfc
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-06 15:20:11 +0500

    enhanced otp generation with model

--- backend/utils/genOTP.js
+++ backend/utils/genOTP.js
@@ -1,5 +1,14 @@
-export default function genOTP() {
-  return parseInt(
+import OTP from "../models/OTP.js";
+
+export default async function genOTP(userId) {
+  const otpCode = parseInt(
     `${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}`,
   );
+  const generatedAt = new Date().toLocaleString();
+  await OTP.create({
+    otpCode: otpCode,
+    generatedAt: generatedAt,
+    userId: userId,
+  });
+  return otpCode;
 }
