a88eb1c77ccfdc987aea127f315108acafc6d7ef
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-06 12:32:57 +0500

    added otp generation utility

---
+++ backend/utils/genOTP.js
@@ -0,0 +1,5 @@
+export default function genOTP() {
+  return parseInt(
+    `${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}`,
+  );
+}
