76f58f8bd95a2629cb1adf97952f989cef7a79ef
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-06 15:09:30 +0500

    created mail transporter

---
+++ backend/utils/mailTransporter.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import nodemailer from "nodemailer";
+
+const gmailUser = process.env.GMAIL_USER;
+const gmailPassword = process.env.GMAIL_PASS;
+
+const mailTransporter = nodemailer.createTransport({
+  secure: true,
+  host: "smtp.gmail.com",
+  port: 465,
+  auth: { user: gmailUser, pass: gmailPassword },
+});
+
+export default mailTransporter;
