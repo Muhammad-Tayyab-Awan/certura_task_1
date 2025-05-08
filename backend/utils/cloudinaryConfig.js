960e22493caf450f39f64cdd5a15d5a1317942c5
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-08 16:45:57 +0500

    refactor: simplify cloudinary configuration export

--- backend/utils/cloudinaryConfig.js
+++ backend/utils/cloudinaryConfig.js
@@ -3,11 +3,11 @@ import cloudinary from "cloudinary";
 const apiKey = process.env.CLOUDINARY_API_KEY;
 const apiSecret = process.env.CLOUDINARY_API_SECRET;
 
-const cloudinaryConfig = cloudinary.v2.config({
+cloudinary.v2.config({
   cloud_name: "dkytvzoqk",
   api_key: apiKey,
   api_secret: apiSecret,
   secure: true,
 });
 
-export default cloudinaryConfig;
+export default cloudinary;
