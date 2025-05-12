0774c9ef0d43fc33c4948c2b6ce3593a8994aa2a
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-12 13:24:42 +0500

    Implemented login route with validation and error handling for user authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -68,4 +68,69 @@ router.post(
   }
 );
 
+router.post(
+  "/login",
+  [
+    body("username").matches(/^[a-z0-9]{6,18}/),
+    body("password")
+      .isStrongPassword({
+        minLowercase: 3,
+        minUppercase: 2,
+        minNumbers: 2,
+        minSymbols: 1,
+        minLength: 8
+      })
+      .isLength({ min: 8, max: 18 })
+  ],
+  async (req, res) => {
+    try {
+      const { userStatus } = req;
+      if (userStatus.loggedIn) {
+        return res.status(400).json({
+          resStatus: false,
+          error: "Invalid request",
+          message: "Please logout first to login to another account"
+        });
+      }
+      const result = validationResult(req);
+      if (!result.isEmpty()) {
+        return res.status(400).json({
+          resStatus: false,
+          error: "Invalid values",
+          message: "Please provide correct values for username and password"
+        });
+      }
+      const { username, password } = req.body;
+      const userCheck = await User.findOne({ username });
+      if (!userCheck) {
+        return res.status(404).json({
+          resStatus: false,
+          error: "Invalid request",
+          message: "Account not found"
+        });
+      }
+      const passwordCheck = bcrypt.compareSync(password, userCheck.password);
+      if (!passwordCheck) {
+        return res.status(400).json({
+          resStatus: false,
+          error: "Invalid request",
+          message: "Please provide correct credentials"
+        });
+      }
+      const auth_token = jwt.sign({ userId: userCheck.id }, jwtSecret);
+      res.cookie("certurat1_auth_token", auth_token, { maxAge: 1.296e9 });
+      res.status(200).json({
+        resStatus: true,
+        message: "Welcome back you logged in successfully"
+      });
+    } catch (error) {
+      res.status(500).json({
+        resStatus: false,
+        error: "Server error found",
+        message: error.message
+      });
+    }
+  }
+);
+
 export default router;
