d070ef4ed050714415747127d7cc545f04720ad1
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-13 10:34:53 +0500

    added main layout to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import MainLayout from "./components/Layout/MainLayout";
 import Router from "./router/route";
 
 function App() {
   return (
-    <>
+    <MainLayout>
       <Router />
-    </>
+    </MainLayout>
   );
 }
 
