7416b639df0e9a2f5f74b3347349f5efb62f55cc
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-13 11:17:43 +0500

    added footer component

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="w-full bg-gray-800 py-2 text-center text-sm text-gray-200">
+      Made with Love and Passion by&nbsp;
+      <a className="underline" href="https://muhammad-tayyab-awan.github.io">
+        Muhammad Tayyab
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
