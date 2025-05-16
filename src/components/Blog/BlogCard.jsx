eeef0e9106e7eada733ff1e1e5af197c0728082e
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-16 17:08:26 +0500

    fix: refactored BlogCard component to use inline style for background image

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -4,7 +4,10 @@ function BlogCard({ title, author, imageLink }) {
   return (
     <Link
       to={title.trim().toLowerCase().replace(/ /g, "-")}
-      className={`relative flex h-56 flex-col justify-end rounded-xl bg-[url('${imageLink}')] bg-amber-500 bg-cover p-4 ring hover:*:first:flex`}
+      className="relative flex h-56 flex-col justify-end rounded-xl bg-amber-500 bg-cover p-4 ring hover:*:first:flex"
+      style={{
+        backgroundImage: `url(${imageLink})`,
+      }}
     >
       <p className="absolute top-0 right-0 hidden rounded-tr-xl rounded-bl-xl bg-gray-200 p-1 text-xs">
         {author}
