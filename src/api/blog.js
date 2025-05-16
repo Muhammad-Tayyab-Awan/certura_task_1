b024965fba0e299fdc1be38b08d53407ee28a3df
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-16 17:03:43 +0500

    feat: implemented blog API with function to fetch all blogs

---
+++ src/api/blog.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+const api_url = import.meta.env.VITE_API_URL;
+
+async function get_all_blogs() {
+  try {
+    let response = await fetch(`${api_url}/api/blog/all`, {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.clear();
+    response = await response.json();
+    return response;
+  } catch (err) {
+    return {
+      resStatus: false,
+      error: "Server not responding",
+      message: "Server not responding please try later",
+    };
+  }
+}
+
+const blogAPI = { get_all_blogs };
+
+export default blogAPI;
