1906c30b3fc589dca59f03ac5ca1508d635bb70c
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-16 15:15:56 +0500

    added signup function to authAPI for user registration

--- src/api/auth.js
+++ src/api/auth.js
@@ -66,6 +66,29 @@ async function logout() {
   }
 }
 
-const authAPI = { login, login_status, logout };
+async function signup(credentials) {
+  try {
+    const JsonCredentials = JSON.stringify({ ...credentials });
+    let response = await fetch(`${api_url}/api/auth/signup`, {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JsonCredentials,
+    });
+    console.clear();
+    response = await response.json();
+    return response;
+  } catch (err) {
+    return {
+      resStatus: false,
+      error: "Server not responding",
+      message: "Server not responding please try later",
+    };
+  }
+}
+
+const authAPI = { login, login_status, logout, signup };
 
 export default authAPI;
