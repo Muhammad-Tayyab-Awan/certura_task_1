f74494df48f6938de1a7b68ded375634336bb3c7
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-16 16:26:58 +0500

    fix: added z-index to Navbar header for proper stacking context

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function Navbar() {
   const [accountModal, setAccountModal] = useState(false);
   return (
-    <header className="sticky top-0 flex h-14 w-full items-center justify-between bg-gray-800 px-2 text-gray-200">
+    <header className="sticky top-0 z-10 flex h-14 w-full items-center justify-between bg-gray-800 px-2 text-gray-200">
       <h1 className="text-lg transition-all duration-700 ease-in-out text-shadow-2xs text-shadow-amber-600 hover:text-shadow-md">
         Write Your Blogs
       </h1>
