972a8714492dbb57999c78fde8ed9892df96013b
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-16 11:46:08 +0500

    added LoginContext for managing user authentication state

---
+++ src/context/LoginContext.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useEffect, useState } from "react";
+import authAPI from "../api/auth";
+
+const LoginContext = createContext({ loggedIn: false, userId: null });
+
+export function LoginContextProvider({ children }) {
+  const [loginState, setLoginState] = useState({
+    loggedIn: false,
+    userId: null,
+  });
+  useEffect(() => {
+    authAPI.login_status().then((response) => {
+      if (response.resStatus) {
+        setLoginState(response.userStatus);
+      }
+    });
+  }, []);
+  return (
+    <LoginContext.Provider value={{ ...loginState, setLoginState }}>
+      {children}
+    </LoginContext.Provider>
+  );
+}
+
+export function useLoginContext() {
+  return useContext(LoginContext);
+}
+
+export default LoginContext;
