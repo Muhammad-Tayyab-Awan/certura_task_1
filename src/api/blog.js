430e960b8e4fbb0ffb56de6137d12f130336db28
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-16 18:08:21 +0500

    feat: added get_blog_by_id function to fetch a single blog by its ID

--- src/api/blog.js
+++ src/api/blog.js
@@ -22,6 +22,27 @@ async function get_all_blogs() {
   }
 }
 
-const blogAPI = { get_all_blogs };
+async function get_blog_by_id(blogId) {
+  try {
+    let response = await fetch(`${api_url}/api/blog/${blogId}`, {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.clear();
+    response = await response.json();
+    return response;
+  } catch (err) {
+    return {
+      resStatus: false,
+      error: "Server not responding",
+      message: "Server not responding please try later",
+    };
+  }
+}
+
+const blogAPI = { get_all_blogs, get_blog_by_id };
 
 export default blogAPI;
