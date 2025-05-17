2189ac20c3983e78c61f4de7d2be5efca4755a85
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-17 12:31:34 +0500

    feat: implemented delete functionality for individual blog posts and update MyBlogs component to conditionally render delete button

--- src/api/blog.js
+++ src/api/blog.js
@@ -64,6 +64,27 @@ async function get_blog_by_id(blogId) {
   }
 }
 
+async function delete_blog_by_id(blogId) {
+  try {
+    let response = await fetch(`${api_url}/api/blog/${blogId}`, {
+      credentials: "include",
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.clear();
+    response = await response.json();
+    return response;
+  } catch (err) {
+    return {
+      resStatus: false,
+      error: "Server not responding",
+      message: "Server not responding please try later",
+    };
+  }
+}
+
 async function create_blog(blogData) {
   try {
     let response = await fetch(`${api_url}/api/blog/create`, {
@@ -113,6 +134,7 @@ const blogAPI = {
   get_my_blogs,
   create_blog,
   delete_all,
+  delete_blog_by_id,
 };
 
 export default blogAPI;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import blogAPI from "../api/blog";
 import toast from "react-hot-toast";
+import { useLoginContext } from "../context/LoginContext";
 
 function Blog() {
   const { blogId } = useParams();
   const [blog, setBlog] = useState("loading");
   const navigate = useNavigate();
+  const { loggedIn, username } = useLoginContext();
   useEffect(() => {
     blogAPI.get_blog_by_id(blogId).then((response) => {
       if (!response.resStatus) {
@@ -37,8 +39,30 @@ function Blog() {
             className="mt-4 w-full rounded-md"
           />
           <p className="mx-auto mt-4 flex w-[75%] justify-between text-sm">
-            <p>Published By : {blog.author.username}</p>
-            <p>Published On : {new Date(blog.createdAt).toLocaleString()}</p>
+            <span>Published By : {blog.author.username}</span>
+            {loggedIn && username === blog.author.username ? (
+              <button
+                className="cursor-pointer rounded-md bg-red-500 px-3 py-1 text-white focus-visible:outline-hidden disabled:animate-pulse disabled:cursor-progress"
+                onClick={async (e) => {
+                  e.target.disabled = true;
+                  const response = await blogAPI.delete_blog_by_id(blogId);
+                  e.target.disabled = false;
+                  if (!response.resStatus) {
+                    toast.error(response.message);
+                    return;
+                  }
+                  toast.success(response.message);
+                  navigate("/my-blogs");
+                }}
+              >
+                Delete
+              </button>
+            ) : (
+              <></>
+            )}
+            <span>
+              Published On : {new Date(blog.createdAt).toLocaleString()}
+            </span>
           </p>
           <p className="mt-4 mb-6 px-5 text-justify leading-loose text-gray-500">
             {blog.content}
