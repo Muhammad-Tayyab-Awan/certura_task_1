401d4204112c1350bf38b618c2aaaef95c81667d
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-17 11:30:24 +0500

    feat: added NotFound component to handle 404 errors with user-friendly message

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+function NotFound() {
+  return (
+    <div className="overflow-y-auto">
+      <h1 className="mt-16 text-center text-xl">Page not found</h1>
+      <p className="mt-5 text-center">
+        Page you are requesting for is not available yet
+      </p>
+    </div>
+  );
+}
+
+export default NotFound;
